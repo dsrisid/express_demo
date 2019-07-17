a63aa48932d9b7b8879c396ebd44c5a4fab4edd8
Srikanth D <srikanth.devanaboina@gmail.com>
2019-07-17 22:53:26 +0530

    Version 4 with req params object

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 Ver 1: First rest api and using the process object to read the env variable
 Ver 2: Adding the second api to support /api/courses endpoint and using lambda function
 Ver 3: Using Req Params..
+Ver 4: request params object
 */
 
 const express = require('express');
@@ -22,6 +23,9 @@ app.get("/api/courses/:id",(req,res)=>{
   res.send(req.params.id);
 });
 
+app.get("/api/posts/:year/:month",(req,res)=>{
+  res.send(req.params);//output of : /api/posts/2018/5 => {"year":"2018","month":"5"}
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>console.log(`Listening on port ${PORT}....`));
