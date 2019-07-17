7a41ed95a8154a25703cb4fd09217fab98fcf3b5
Srikanth D <srikanth.devanaboina@gmail.com>
2019-07-17 22:27:01 +0530

    Version 2 with new api exposed and using lamda function

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get("/",function(req,res){
+app.get("/",(req,res)=>{
   res.send("Hello");
 });
+
+app.get("/api/courses",(req,res)=>{
+  res.send([1,2,3]);
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>console.log(`Listening on port ${PORT}....`));
