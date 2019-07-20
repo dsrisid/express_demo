334171ac2e2ba5c643128d6dcc0134f35a5f8635
Srikanth D <srikanth.devanaboina@gmail.com>
2019-07-20 17:15:34 +0530

    joi module for data validation

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ Ver 5: Query Params
 Ver 6 :using courses array and req param using http status code
 Ver 7 :Post and app.use to understand json structures...
 Ver 8: basic data validation
+Ver 9: Using @hapi/joi module for data validation
 */
 const express = require('express');
 const app = express();
-
+const Joi = require('@hapi/joi');
 //We are adding a piece of MW here which will help to undertsand the json structure used during post etc..
 app.use(express.json());
 var courses = [
@@ -32,7 +33,15 @@ app.get("/api/courses/:id",(req,res)=>{
 });
 
 app.post("/api/courses",(req,res)=>{
-  if(!req.body.name || req.body.name.length < 3){
+  const schema = Joi.object().keys({
+    name:Joi.string().min(3).required()
+  });
+  const result = Joi.validate(req.body,schema);
+  // if(!req.body.name || req.body.name.length < 3){
+  //   res.status(400).send("Name is required and should be minimum three characters");
+  //   return;
+  // }
+  if(result.error){
     res.status(400).send("Name is required and should be minimum three characters");
     return;
   }
