18d8247960cd9a4de4388fe50ae9da8522deeb8e
Srikanth D <srikanth.devanaboina@gmail.com>
2019-07-21 09:48:39 +0530

    refactoring to simplify if blocks

--- index.js
+++ index.js
@@ -30,16 +30,14 @@ app.get("/api/courses",(req,res)=>{
 
 app.get("/api/courses/:id",(req,res)=>{
   const course = courses.find(c=>c.id === parseInt(req.params.id));
-  if(!course) res.status(404).send(`course with id: ${req.params.id} not found`);
+  if(!course) return res.status(404).send(`course with id: ${req.params.id} not found`);
   res.send(course);
 });
 
 app.post("/api/courses",(req,res)=>{
   const {error} = validateCourse(req.body);
-  if(error){
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if(error) return res.status(400).send(error.details[0].message);
+
   const course = {"id":courses.length+1, "name":req.body.name};
   courses.push(course);
   res.send(course)
@@ -47,10 +45,7 @@ app.post("/api/courses",(req,res)=>{
 
 app.put("/api/courses/:id",(req,res)=>{
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if(!course){
-    res.status(404).send(`course with id ${req.params.id} is not found`);
-    return;
-  }
+  if(!course) return res.status(404).send(`course with id ${req.params.id} is not found`);
 
   // const result = validateCourse(course);
   // if(result.error){
@@ -59,20 +54,14 @@ app.put("/api/courses/:id",(req,res)=>{
   // }
   const {error} = validateCourse(req.body);//Instead of using the whole object as we are interested in only the error
   //property we could get that by the above notation...This is called object destructuring feature.
-  if(error){
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if(error) return res.status(400).send(error.details[0].message);
   course.name = req.body.name;
   res.send(course)
 });
 
 app.delete("/api/courses/:id",(req,res)=>{
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if(!course){
-    res.status(404).send(`course with id ${req.params.id} is not found`);
-    return;
-  }
+  if(!course) return res.status(404).send(`course with id ${req.params.id} is not found`);
   const index = courses.indexOf(course);
   courses.splice(index,1);
   res.send(course);
